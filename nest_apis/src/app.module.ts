d4bb98d1311f72b9e2c0a4259e5642bf1476bf92
wz2cool <wz2cool@live.cn>
2018-04-16 11:26:50 +0800

    add

--- nest_apis/src/app.module.ts
+++ nest_apis/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './controller/app.controller';
 
 @Module({
   imports: [],
   controllers: [AppController],
   components: [],
 })
-export class AppModule {}
+export class AppModule { }
