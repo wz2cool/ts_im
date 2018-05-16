da5b60e62a2c5de52d9850964ee7e3b859499d3d
wz2cool <wz2cool@live.cn>
2018-05-16 23:10:18 +0800

    add

---
+++ nest_apis/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var sourcemaps = require("gulp-sourcemaps");
+var tsProject = ts.createProject("tsconfig.json");
+var del = require('del');
+
+function move(src, dest) {
+    return gulp.src(src)
+        .pipe(gulp.dest(dest));
+};
+
+gulp.task('clean', function () {
+    return del(['dist']);
+});
+
+gulp.task("build", ['clean'], function () {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject()).js
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('default', ['build']);
