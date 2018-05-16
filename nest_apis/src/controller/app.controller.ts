d79c9974b18a75565e4cb8e515bccb3bd5abd6b2
wz2cool <wz2cool@live.cn>
2018-05-16 16:38:19 +0800

    add

--- nest_apis/src/controller/app.controller.ts
+++ nest_apis/src/controller/app.controller.ts
@@ -1,9 +1,15 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Post, Body, Request, Response } from '@nestjs/common';
+import { Student } from '../model/student';
 
 @Controller()
 export class AppController {
   @Get()
   root(): string {
     return 'Hello World!';
+  }
+
+  @Post()
+  addStudent(@Body() newStudent: Student): Student {
+    return null;
   }
 }
