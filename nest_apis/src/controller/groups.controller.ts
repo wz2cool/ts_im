824f128fc609a920b6f46498c9fa7ebe378dbed2
wz2cool <wz2cool@live.cn>
2018-05-18 16:54:21 +0800

    add

--- nest_apis/src/controller/groups.controller.ts
+++ nest_apis/src/controller/groups.controller.ts
@@ -1,28 +1,38 @@
-import { Get, Controller, Post, Body, Put, Request, Response, Param } from '@nestjs/common';
+import { Get, Controller, Post, Body, Put, Request, Response, Param, Query, UseInterceptors } from '@nestjs/common';
 import { CreateGroupDto, UpdateGroupDto, GroupPageDto } from '../model/dto';
 import { GroupDbService } from '../service';
-import { ApiImplicitParam, ApiResponse } from '@nestjs/swagger';
+import { ApiImplicitParam, ApiResponse, ApiImplicitQuery } from '@nestjs/swagger';
+import { LoggingInterceptor } from '../common/interceptor';
 
 @Controller('groups')
+@UseInterceptors(LoggingInterceptor)
 export class GroupsController {
     constructor(private readonly groupDbService: GroupDbService) {
     }
 
     @Post()
-    @ApiResponse({ status: 200, description: '返回插入的ID', type: 'Integer' })
+    @ApiResponse({ status: 200, description: '返回插入的ID', type: 'integer' })
     async create(@Body() dto: CreateGroupDto) {
         return await this.groupDbService.createGroup(dto);
     }
 
     @Put(':groupId')
-    @ApiImplicitParam({ name: 'groupId', type: Number })
+    @ApiImplicitParam({ name: 'groupId', type: 'integer' })
     async update(@Param() params: any, @Body() dto: UpdateGroupDto) {
         return await this.groupDbService.updateGroup(params.groupId, dto);
     }
 
     @Get()
-    @ApiResponse({ status: 200, description: '获取全部组', type: GroupPageDto })
-    async getGroups() {
+    @ApiImplicitQuery({ name: 'pageNum', type: 'integer' })
+    @ApiImplicitQuery({ name: 'pageSize', type: 'integer' })
+    @ApiResponse({ status: 200, description: '分页获取全部组', type: GroupPageDto })
+    async getGroups(@Query() query: any) {
+        console.log(query);
+    }
 
+    @Get(':groupId')
+    @ApiImplicitParam({ name: 'groupId', type: 'integer' })
+    async getGroupById(@Param() params: any) {
+        console.log(params);
     }
 }
