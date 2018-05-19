1a8ccd82eb3583380cb56702b3e51ce88e5f34f4
wz2cool <wz2cool@live.cn>
2018-05-19 19:14:17 +0800

    add

--- nest_apis/src/service/db-core.service.ts
+++ nest_apis/src/service/db-core.service.ts
@@ -1,12 +1,20 @@
 import { Component } from '@nestjs/common';
-import { ConnectionFactory, MysqlConnectionConfig, IConnection } from 'tsbatis';
+import { ConnectionFactory, MysqlConnectionConfig, IConnection, CommonHelper } from 'tsbatis';
 
 @Component()
 export class DbCoreService {
     readonly connectionFactory;
     constructor() {
         console.log('DbCoreService init');
-        const enableDatabaseLog: boolean = process.env.ENABLE_DATABASE_LOG.toLowerCase() === 'true';
+        console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+        console.log(`ENABLE_DATABASE_LOG: ${process.env.ENABLE_DATABASE_LOG}`);
+
+        let enableDatabaseLog = false;
+        if (!CommonHelper.isNullOrUndefined(process.env.ENABLE_DATABASE_LOG)) {
+            enableDatabaseLog =
+                process.env.ENABLE_DATABASE_LOG.toLocaleLowerCase().trim() === 'true';
+        }
+
         console.log('enable database log: ' + enableDatabaseLog);
         const config = new MysqlConnectionConfig();
         config.database = process.env.DATABASE_DB;
@@ -14,6 +22,9 @@ export class DbCoreService {
         config.port = Number.parseInt(process.env.DATABASE_PORT);
         config.user = process.env.DATABASE_USER;
         config.password = process.env.DATABASE_PWD;
+
+        console.log('MysqlConfig ' + JSON.stringify(config));
+
         this.connectionFactory = new ConnectionFactory(config, enableDatabaseLog);
     }
 
