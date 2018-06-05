7836460212da69deacd9c5e1bf03549eb62a247f
wz2cool <wz2cool@live.cn>
2018-06-05 10:03:07 +0800

    add

--- ui/management/src/routes/UserManagement/modules/UserManagement.ts
+++ ui/management/src/routes/UserManagement/modules/UserManagement.ts
@@ -1,33 +1,56 @@
+import * as lodash from 'lodash';
+
+import { UserInfoPageDto } from "../../../models/dto";
+
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const USER_MANAGEMENT = "USER_MANAGEMENT";
+export const FETCH_USER_INFO_PAGE_BEGIN = "FETCH_USER_INFO_PAGE_BEGIN";
+export const FETCH_USER_INFO_PAGE_SUCCESS = "FETCH_USER_INFO_PAGE_SUCCESS";
 
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function userManagement(value = "USER_MANAGEMENT") {
+export function fetchUserInfoPageBegin(value = true) {
   return {
-    type: USER_MANAGEMENT,
+    type: FETCH_USER_INFO_PAGE_BEGIN,
+    payload: value,
+  };
+}
+
+export function fetchUserInfoPageSuccess(value: UserInfoPageDto) {
+  return {
+    type: FETCH_USER_INFO_PAGE_SUCCESS,
     payload: value,
   };
 }
 
 export const actions = {
-  userManagement,
+  fetchUserInfoPageBegin,
 };
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [USER_MANAGEMENT]: (state: any, action: any) => action.payload,
+  [FETCH_USER_INFO_PAGE_BEGIN]: (state: UserManagementState, action: any) => { lodash.assign(state, { loading: action.payload }) },
 };
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = "";
+export interface UserManagementState {
+  loading: boolean;
+  userInfoPage?: UserInfoPageDto;
+  error?: Error;
+}
+
+const initialState: UserManagementState = {
+  loading: false,
+  userInfoPage: undefined,
+  error: undefined,
+};
+
 export function reducer(state = initialState, action: any) {
   const handler = ACTION_HANDLERS[action.type];
 
