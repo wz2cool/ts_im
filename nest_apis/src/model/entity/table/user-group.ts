30659e86837b4122473d798c14ee6ced129ab124
wz2cool <wz2cool@live.cn>
2018-06-20 11:03:47 +0800

    add

--- nest_apis/src/model/entity/table/user-group.ts
+++ nest_apis/src/model/entity/table/user-group.ts
@@ -1,23 +1,22 @@
 import { column, TableEntity } from 'tsbatis';
 
 export class UserGroup extends TableEntity {
+  @column('id', true, true)
+  id: number;
+  @column('group_id', false, false)
+  groupId: number;
+  @column('user_id', false, false)
+  userId: number;
+  @column('user_group_category_id', false, false)
+  userGroupCategoryId: number;
+  @column('display_name', false, false)
+  displayName: string;
+  @column('create_time', false, false)
+  createTime: Date;
+  @column('update_time', false, false)
+  updateTime: Date;
 
-    @column('id', true, true)
-    public id: number;
-    @column('group_id', false, false)
-    public groupId: number;
-    @column('user_id', false, false)
-    public userId: number;
-    @column('user_group_category_id', false, false)
-    public userGroupCategoryId: number;
-    @column('display_name', false, false)
-    public displayName: string;
-    @column('create_time', false, false)
-    public createTime: Date;
-    @column('update_time', false, false)
-    public updateTime: Date;
-
-    getTableName(): string {
-        return 'user_group';
-    }
+  getTableName(): string {
+    return 'user_group';
+  }
 }
