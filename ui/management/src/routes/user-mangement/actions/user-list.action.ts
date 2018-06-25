79a2e4337f1731d3df00c7cb4d5f567ce562a7d2
wz2cool <wz2cool@live.cn>
2018-06-25 16:39:35 +0800

    add

---
+++ ui/management/src/routes/user-mangement/actions/user-list.action.ts
@@ -0,0 +1,69 @@
+import { ActionPayload } from "../../../models/interface";
+import { UserInfoPageDto, UserFilterDto } from "../../../models/dto";
+import { Dispatch } from "redux";
+import { UserHttpService } from "../../../services";
+
+export const FETCH_USER_INFO_PAGE_BEGIN = "FETCH_USER_INFO_PAGE_BEGIN";
+export const FETCH_USER_INFO_PAGE_SUCCESS = "FETCH_USER_INFO_PAGE_SUCCESS";
+export const FETCH_USER_INFO_PAGE_FAILED = "FETCH_USER_INFO_PAGE_FAILED";
+export const PAGE_NUM_CHANGE = "PAGE_NUM_CHANGE";
+export const PAGE_SIZE_CHANGE = "PAGE_SIZE_CHANGE";
+export const SEARCH_FIELD_CHANGE = "SEARCH_FIELD_CHANGE";
+
+const userHttpService = UserHttpService.getInstance();
+
+export const fetchUserInfoPageBegin = (): ActionPayload => {
+  return {
+    type: FETCH_USER_INFO_PAGE_BEGIN,
+  };
+};
+
+export const fetchUserInfoPageSuccess = (value: UserInfoPageDto): ActionPayload => {
+  return {
+    type: FETCH_USER_INFO_PAGE_SUCCESS,
+    payload: value,
+  };
+};
+
+export const fetchUserInfoPageFailed = (value: Error): ActionPayload => {
+  return {
+    type: FETCH_USER_INFO_PAGE_FAILED,
+    payload: value,
+  };
+};
+
+export const pageNumChange = (value: number): ActionPayload => {
+  return {
+    type: PAGE_NUM_CHANGE,
+    payload: value,
+  };
+};
+
+export const pageSizeChange = (value: number): ActionPayload => {
+  return {
+    type: PAGE_SIZE_CHANGE,
+    payload: value,
+  };
+};
+
+export const searchFieldChange = (filter: UserFilterDto): ActionPayload => {
+  console.log(filter);
+  return {
+    type: SEARCH_FIELD_CHANGE,
+    payload: filter,
+  };
+};
+
+export const fetchUserInfoPage = (filter: UserFilterDto, pageNum: number, pageSize: number) => {
+  return (dispatch: Dispatch<any>) => {
+    dispatch(fetchUserInfoPageBegin());
+    return userHttpService
+      .getUserInfosByFilter(filter, pageNum, pageSize)
+      .then(response => {
+        dispatch(fetchUserInfoPageSuccess(response.data));
+      })
+      .catch(err => {
+        dispatch(fetchUserInfoPageFailed(err));
+      });
+  };
+};
