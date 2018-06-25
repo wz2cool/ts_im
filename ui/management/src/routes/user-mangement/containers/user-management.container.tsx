2b593b85cd61dd9d569bab6c7c1563ae0061dbad
wz2cool <wz2cool@live.cn>
2018-06-25 16:30:07 +0800

    add

--- ui/management/src/routes/user-mangement/containers/user-management.container.tsx
+++ ui/management/src/routes/user-mangement/containers/user-management.container.tsx
@@ -1,47 +1,18 @@
 import { withRouter } from "react-router-dom";
+import * as React from "react";
 import { connect } from "react-redux";
-import UserManagement, {
-  StateToProps,
-  DispatchToProps,
-} from "../components/user-management";
-import {
-  fetchUserInfoPage,
-  ReducerState,
-  pageNumChange,
-  pageSizeChange,
-  searchFieldChange,
-} from "../modules/UserManagement";
-import { UserFilterDto } from "../../../models/dto";
+import { RouteComponentProps } from "react-router-dom";
 
-const mapStateToProps = (state: any): StateToProps => {
-  const myState: ReducerState = state.userManagement;
-  return {
-    loading: myState.loading,
-    userInfoPage: myState.userInfoPage,
-    userFilter: myState.userFilter,
-    pageNum: myState.pageNum,
-    pageSize: myState.pageSize,
-    error: myState.error,
-  };
-};
+export interface UserManagementProps extends RouteComponentProps<any> {}
 
-const mapDispatchToProps = (dispatch: any): DispatchToProps => {
-  return {
-    fetchUserInfoPage: (
-      filter: UserFilterDto,
-      pageNum: number,
-      pageSize: number,
-    ) => dispatch(fetchUserInfoPage(filter, pageNum, pageSize)),
-    pageNumChange: (pageNum: number) => dispatch(pageNumChange(pageNum)),
-    pageSizeChange: (pageSize: number) => dispatch(pageSizeChange(pageSize)),
-    searchFieldChange: (filter: UserFilterDto) =>
-      dispatch(searchFieldChange(filter)),
-  };
-};
+interface UserManagementState {}
 
-const UserManagementContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(UserManagement);
+class UserManagement extends React.Component<UserManagementProps, UserManagementState> {
+  public render(): JSX.Element {
+    return <span>UserManagement</span>;
+  }
+}
+
+const UserManagementContainer = connect()(UserManagement);
 
 export default withRouter(UserManagementContainer);
